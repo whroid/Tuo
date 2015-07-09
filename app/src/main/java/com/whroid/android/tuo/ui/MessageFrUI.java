55e11dbee2cb28db797d9e11169b70559fbdda1f
whroid <whroid@gmail.com>
2015-07-09 20:05:46 +0800

    提交1

--- app/src/main/java/com/whroid/android/tuo/ui/MessageFrUI.java
+++ app/src/main/java/com/whroid/android/tuo/ui/MessageFrUI.java
@@ -1,12 +1,14 @@
 package com.whroid.android.tuo.ui;
 
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.widget.TextView;
 import com.whroid.android.tuo.R;
 import com.whroid.android.tuo.base.ui.TFragment;
 
 public class MessageFrUI extends TFragment{
 
+    TextView tv_message;
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
@@ -16,13 +18,22 @@ public class MessageFrUI extends TFragment{
 	@Override
 	public void initView() {
 		// TODO Auto-generated method stub
-		
+		tv_message = (TextView) findViewById(R.id.message);
 	}
 
 	@Override
 	public void initData() {
 		// TODO Auto-generated method stub
-		
+
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+
+              //  String str = UrlConnectHttpUtil.test();
+               // tv_message.setText(str);
+            }
+        });
+
 	}
 
 	@Override
